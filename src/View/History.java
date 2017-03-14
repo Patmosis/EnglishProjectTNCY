5480cc03be5f3000cba84d5256fce780a648be52
NicolasDub <nicolas.dubois@telecomnancy.net>
2017-03-14 17:19:14 +0100

    Update History.java
    modif

--- src/View/History.java
+++ src/View/History.java
@@ -1,25 +1,105 @@
 package View;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
-/**
- * Main pane of the history part of the application
- * @author nicolas
- *
- */
 public class History extends BorderPane {
 	
-	public History() {
+	private static int nbPages = 15;
+	private int numPage = 0;
+	
+	
+	public History(int numPage) {
 		initialize();
 	}
 	
-	/**
-	 * Initializes history pane
-	 */
-	private void initialize() {
-		// TODO Create history pane
-		setCenter(new Label("History"));
+	
+	public void initialize() {
+		
+		Label title = new Label();
+		VBox content = new VBox();
+		StackPane change = new StackPane();
+		Button prev = new Button("previous\n🢀");
+		Button next = new Button("next\n    🢂");
+		Button start = new Button("start");
+		
+		getChildren().removeAll();
+		
+		prev.getStyleClass().add("backButton");
+		next.getStyleClass().add("backButton");
+		
+		prev.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+            	numPage--;
+            	initialize();
+            }
+        });
+		
+		next.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) {
+				numPage++;
+				initialize();
+			}
+		});
+		
+		
+		if(numPage == 0) {
+			
+			start.getStyleClass().add("startButton");
+			
+			start.setOnAction(new EventHandler<ActionEvent>() {
+				public void handle(ActionEvent e) {
+					numPage++;
+					initialize();
+				}
+			});
+			
+			change.getChildren().add(start);
+			StackPane.setAlignment(start, Pos.BOTTOM_CENTER);
+		
+		}
+		
+		if(numPage == 1) {
+			
+			next.getStyleClass().add("backButton");
+
+			change.getChildren().add(next);
+			StackPane.setAlignment(next, Pos.BOTTOM_RIGHT);
+
+		}
+		
+		if(numPage < nbPages && numPage > 1) {
+
+			change.getChildren().add(next);
+			StackPane.setAlignment(next, Pos.BOTTOM_RIGHT);
+
+			change.getChildren().add(prev);
+			StackPane.setAlignment(prev, Pos.BOTTOM_LEFT);
+			
+
+		}
+		
+		
+		if(numPage == nbPages) {
+
+			change.getChildren().add(prev);
+			StackPane.setAlignment(prev, Pos.BOTTOM_LEFT);
+			
+		}
+					
+		title.setMaxWidth(Double.MAX_VALUE);
+		title.setAlignment(Pos.CENTER);
+		
+		setTop(title);
+		setCenter(content);
+		setBottom(change);
+		
 	}
 	
 }
